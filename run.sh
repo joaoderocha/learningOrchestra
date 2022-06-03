8d4fc0b55a2871d157b5885267f57ebfa2964c41
João Araújo <jharocha@gmail.com>
2022-06-03 17:13:07 -0300

    fix: inclui mais debugs

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ echo "--------------------------------------------------------------------"
 docker build --tag spark_task microservices/spark_task_image
 docker push 127.0.0.1:5050/spark_task
 
-docker-compose build
+docker-compose build distributed_training
+docker-compose build images
 
 echo "--------------------------------------------------------------------"
 echo "Adding the microservice images in docker daemon security exception..."
@@ -37,86 +38,6 @@ echo "--------------------------------------------------------------------"
 
 sleep 30
 
-
-database_api_repository=127.0.0.1:5050/database_api
-
-
-echo "--------------------------------------------------------------------"
-echo "Pushing databaseApi microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $database_api_repository
-
-
-spark_repository=127.0.0.1:5050/spark
-
-echo "--------------------------------------------------------------------"
-echo "Pushing spark image..."
-echo "--------------------------------------------------------------------"
-docker push $spark_repository
-
-
-projection_repository=127.0.0.1:5050/projection
-
-echo "--------------------------------------------------------------------"
-echo "Pushing projection microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $projection_repository
-
-
-builder_repository=127.0.0.1:5050/builder
-
-echo "--------------------------------------------------------------------"
-echo "Pushing builder microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $builder_repository
-
-
-data_type_handler_repository=127.0.0.1:5050/data_type_handler
-
-echo "--------------------------------------------------------------------"
-echo "Pushing dataTypeHandler microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $data_type_handler_repository
-
-
-histogram_repository=127.0.0.1:5050/histogram
-
-echo "--------------------------------------------------------------------"
-echo "Pushing histogram microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $histogram_repository
-
-
-model_repository=127.0.0.1:5050/model
-
-echo "--------------------------------------------------------------------"
-echo "Pushing model microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $model_repository
-
-
-binary_executor_repository=127.0.0.1:5050/binary_executor
-
-echo "--------------------------------------------------------------------"
-echo "Pushing binaryExecutor microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $binary_executor_repository
-
-
-database_executor_repository=127.0.0.1:5050/database_executor
-
-echo "--------------------------------------------------------------------"
-echo "Pushing databaseExecutor microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $database_executor_repository
-
-
-code_executor_repository=127.0.0.1:5050/code_executor
-
-echo "--------------------------------------------------------------------"
-echo "Pushing codeExecutor microservice image..."
-echo "--------------------------------------------------------------------"
-docker push $code_executor_repository
 
 distributed_training_repository=127.0.0.1:5050/distributed_training
 
