9b50150a0147555f3969d5d93039bf138dabfac5
João Araújo <jharocha@gmail.com>
2022-06-04 09:43:42 -0300

    fix: expoem object e node manager

--- microservices/distributed_training_image/start.sh
+++ microservices/distributed_training_image/start.sh
@@ -2,7 +2,7 @@
 
 cd /home/ray
 
-ray start --head --port=6379 --dashboard-port=8265
+ray start --head --port=6379 --object-manager-port=12345 --node-manager-port=12346 --dashboard-port=8265
 
 cd /usr/src/distributed_training
 
--- microservices/ray_worker_image/start.sh
+++ microservices/ray_worker_image/start.sh
@@ -2,6 +2,6 @@
 
 echo "$RAYHEAD"
 
-ray start --address="$RAYHEAD":6379
+ray start --address="$RAYHEAD":6379 --object-manager-port=12345 --node-manager-port=12346
 
 tail -f /dev/null
