6de1da7cfeb3e91bb5f2237eee4e779e4c9a4564
João Araújo <jharocha@gmail.com>
2022-06-04 13:15:02 -0300

    fix: altera start.sh

--- microservices/ray_worker_image/start.sh
+++ microservices/ray_worker_image/start.sh
@@ -2,4 +2,6 @@
 
 echo "$RAYHEAD"
 
-ray start --address="$RAY_HEAD":"$HOST_PORT" --object-manager-port="$OBJECT_MANAGER_PORT" --node-manager-port="$NODE_MANAGER_PORT" --block
+ray start --address="$RAY_HEAD":"$HOST_PORT" --object-manager-port="$OBJECT_MANAGER_PORT" --node-manager-port="$NODE_MANAGER_PORT"
+
+tail -f /dev/null
