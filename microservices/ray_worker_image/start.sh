78d047cf3d89479c70cb7715a4e15e748c403c7d
João Araújo <jharocha@gmail.com>
2022-06-04 11:00:33 -0300

    fix: remapeia portas client server

--- microservices/ray_worker_image/start.sh
+++ microservices/ray_worker_image/start.sh
@@ -2,6 +2,4 @@
 
 echo "$RAYHEAD"
 
-ray start --address="$RAYHEAD":6379 --object-manager-port=12345 --node-manager-port=12346
-
-tail -f /dev/null
+ray start --address="$RAYHEAD":6379 --object-manager-port=12345 --node-manager-port=12346 --block
