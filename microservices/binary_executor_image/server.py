158829b8342069e091a0eb7b4f9b2e99faf8088c
João Araújo <jharocha@gmail.com>
2022-06-11 11:35:13 -0300

    fix: timeout

--- microservices/binary_executor_image/server.py
+++ microservices/binary_executor_image/server.py
@@ -16,7 +16,7 @@ ray.init(address=address, runtime_env=runtime_env)
 
 hvd.init()
 
-settings = RayExecutor.create_settings(timeout_s=300, placement_group_timeout_s=300)
+settings = RayExecutor.create_settings(timeout_s=60, placement_group_timeout_s=60)
 executor = RayExecutor(settings, num_workers_per_host=1, num_hosts=2, use_gpu=False, cpus_per_worker=2)
 executor.start()
 
